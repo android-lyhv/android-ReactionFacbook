93adaf04cad0796f6c59aab992fdfb024f77a935
ly.ho <ly.ho@asiantech.vn>
2017-07-14 13:53:30 +0700

    add reaction icon

---
+++ app/src/main/java/com/hovanly/dut/reactionfacebook/ReactionView.java
@@ -0,0 +1,20 @@
+package com.hovanly.dut.reactionfacebook;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Copyright@ AsianTech.Inc
+ * Created by Ly Ho V. on 14/07/2017
+ */
+public class ReactionView extends View {
+    public ReactionView(Context context) {
+        super(context);
+    }
+
+    public ReactionView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+}
